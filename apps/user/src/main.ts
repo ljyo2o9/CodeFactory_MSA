f4a8c9a6ba75c81f4e8d15611f88a723f0a70520
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-07 16:38:33 +0900

    feat(auth.controller.ts): 사용자 등록 및 로그인에 유효성 검사 추가 refactor(main.ts): 전역 유효성 검사 파이프 제거

--- apps/user/src/main.ts
+++ apps/user/src/main.ts
@@ -4,7 +4,6 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe())
   await app.listen(process.env.HTTP_PORT ?? 3000);
 }
 bootstrap();
