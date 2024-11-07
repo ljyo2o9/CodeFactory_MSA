53a000982ad873436521aea1119f77fff50389fb
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-08 08:22:39 +0900

    feat(app.module.ts): 환경 변수 및 TypeORM 설정 추가 환경 변수를 검증하고 TypeORM을 비동기로 설정하여 데이터베이스 연결을 개선하기 위해 변경함.

--- apps/product/src/app.module.ts
+++ apps/product/src/app.module.ts
@@ -1,7 +1,34 @@
 import { Module } from '@nestjs/common';
 import { ProductModule } from './product/product.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        entities: [],
+        database: 'db',
+        synchronize: true,
+      }),
+    }),
+    ProductModule],
 })
 export class AppModule {}
