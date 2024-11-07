c67d8b67dcd4d7c2abb1cbf9863f0243237473df
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-07 10:53:07 +0900

    chore: ESLint 설정에 추가 구성 추가 및 prettier 버전 업데이트

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    "some-other-config-you-use", "prettier",
   ],
   root: true,
   env: {
