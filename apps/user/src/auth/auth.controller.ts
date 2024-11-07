654cc07126ccae790053c8a117b9acfdbce5ac46
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-07 16:56:44 +0900

    feat(auth): 로그인 시 토큰 검증 및 JWT 발급 기능 추가 feat(auth): AuthService에 사용자 인증 및 토큰 발급 메서드 추가 fix(auth.controller): 로그인 시 토큰이 없을 경우 예외 처리 추가 chore(auth.module): TypeOrm 및 JwtModule 의존성 추가

--- apps/user/src/auth/auth.controller.ts
+++ apps/user/src/auth/auth.controller.ts
@@ -23,5 +23,11 @@ export class AuthController {
 
   @Post('login')
   @UsePipes(ValidationPipe)
-  loginUser(@Authorization() token: string) {}
+  loginUser(@Authorization() token: string) {
+    if (token === null) {
+      throw new UnauthorizedException('토큰을 입력해주세요!');
+    }
+
+    return this.authService.login(token);
+  }
 }
--- apps/user/src/auth/auth.module.ts
+++ apps/user/src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UserModule } from '../user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from '../user/entity/user.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({}),
+    UserModule,
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
