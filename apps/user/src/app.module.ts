d1bf4b464570c8b3caea232eba80aaf06a14bc28
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-07 15:55:52 +0900

    feat(app.module.ts): 환경 변수 및 데이터베이스 설정 추가 - ConfigModule을 추가하여 전역 환경 변수 설정을 지원 - Joi를 사용하여 환경 변수 유효성 검사를 추가 - TypeOrmModule을 비동기로 설정하여 데이터베이스 연결 구성

--- apps/user/src/app.module.ts
+++ apps/user/src/app.module.ts
@@ -1,8 +1,38 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from 'joi';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/entity/user.entity';
 
 @Module({
-  imports: [UserModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        HTTP_PORT: Joi.string().required(),
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+      }),
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        entities: [User],
+        database: 'db',
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
