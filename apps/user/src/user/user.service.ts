fb46955c6947fd295be0f4287c6ff8f3c42d20ac
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-07 15:56:55 +0900

    feat(user): 사용자 생성 기능 및 DTO, 엔티티 추가 사용자 생성 시 이메일 중복 확인 및 비밀번호 해시 처리 추가

--- apps/user/src/user/user.service.ts
+++ apps/user/src/user/user.service.ts
@@ -1,8 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { User } from './entity/user.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const { email, password, age, name, profile } = createUserDto;
+
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (user) {
+      throw new BadRequestException('이미 가입한 이메일 입니다!');
+    }
+
+    const hash = await bcrypt.hash(password, 10);
+
+    await this.userRepository.save({
+      email,
+      password: hash,
+      age,
+      name,
+      profile,
+    });
+
+    return this.userRepository.findOne({
+      where: {
+        email,
+      },
+    });
   }
 }
