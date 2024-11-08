6960d85ebd51a266050e1b7a46ce69d4fb78cd88
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-08 13:40:56 +0900

    feat(app.module.ts): TypeORM에서 Mongoose로 데이터베이스 모듈 변경 변경 사항: 데이터베이스 연결을 MySQL에서 MongoDB로 전환하여 유연성과 확장성을 개선함.

--- apps/order/src/app.module.ts
+++ apps/order/src/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { OrderModule } from './order/order.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,24 +10,14 @@ import { OrderModule } from './order/order.module';
       isGlobal: true,
       validationSchema: Joi.object({
         HTTP_PORT: Joi.string().required(),
-        DB_HOST: Joi.string().required(),
-        DB_PORT: Joi.number().required(),
-        DB_USERNAME: Joi.string().required(),
-        DB_PASSWORD: Joi.string().required(),
+        DB_URL: Joi.string().required(),
       }),
     }),
-    TypeOrmModule.forRootAsync({
+    MongooseModule.forRootAsync({
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        host: configService.get<string>('DB_HOST'),
-        port: configService.get<number>('DB_PORT'),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        entities: [],
-        database: 'db',
-        synchronize: true,
-      }),
+        uri: configService.getOrThrow('DB_URL'),
+      })
     }),
     OrderModule,
   ],
