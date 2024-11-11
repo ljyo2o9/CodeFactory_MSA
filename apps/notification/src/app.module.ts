63413da91e0443e093507b20dab2795c4287b1ff
ljyo2o9 <yggosxoljy@gmail.com>
2024-11-11 20:28:29 +0900

    feat(notification): Dockerfile 추가 및 MongoDB 설정 feat(app.module.ts): 환경 변수 및 MongoDB 연결 설정 추가 chore(docker-compose): notification 서비스 및 MongoDB 설정 추가

--- apps/notification/src/app.module.ts
+++ apps/notification/src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
 import { NotificationModule } from './notification/notification.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [NotificationModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_URL: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.getOrThrow('DB_URL'),
+      }),
+    }),
+    NotificationModule,
+  ],
 })
 export class AppModule {}
